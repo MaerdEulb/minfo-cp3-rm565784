92f4ea0cc448f37248f2133adec22db7fc5cfb2e
MaerdEulb <defranca06@outlook.com>
2025-05-23 07:51:23 -0300

    minfo.sh
    criação do arquivo.

---
+++ minfo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# minfo.sh - Script para mostrar informações do sistema
+
+if [ $# -ne 1 ]; then
+    echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+    exit 1
+fi
+
+case "$1" in
+    hostname)
+        hostname
+        ;;
+    uptime)
+        uptime -p
+        ;;
+    disk)
+        df -h /
+        ;;
+    all)
+        hostname
+        uptime -p
+        df -h /
+        ;;
+    *)
+        echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+        exit 1
+        ;;
+esac
